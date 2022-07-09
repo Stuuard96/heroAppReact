e33905fb168db0f48c77b86d4714efba33a1ec0f
Stuard Quiroz <stuuard96@gmail.com>
2022-07-08 20:30:03 -0500

    Se agrego 2 rutas - search y hero

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import { DcPage, MarvelPage } from "../heroes";
+import { HeroesRoutes } from "../heroes";
 import { LoginPage } from "../auth";
-import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DcPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="/" element={<Navigate to="/marvel" />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="login" element={<LoginPage />} />
+      <Route path="/*" element={<HeroesRoutes />} />
+    </Routes>
   );
 };
